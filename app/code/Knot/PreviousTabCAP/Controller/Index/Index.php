e3cccf1e178bea6465d54120536c3ee6eaae6194
Bart Knot <bart.knot@gmail.com>
2018-10-30 11:29:35 +0100

    working tab

--- app/code/Knot/PreviousTabCAP/Controller/Index/Index.php
+++ app/code/Knot/PreviousTabCAP/Controller/Index/Index.php
@@ -8,9 +8,6 @@ class Index extends \Magento\Framework\App\Action\Action
 {
     public function execute()
     {
-        /** @var \Magento\Framework\Controller\Result\Raw $result */
-        $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
-        $result->setContents('Test');
-        return $result;
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
